e3af3a0248524706445456137d3978d91a60fd1a
pgomesdev <pgomesdev@gmail.com>
2019-06-26 20:15:42 -0300

    feat: create user endpoint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
   },
   rules: {
     'no-param-reassign': 0,
+    'class-methods-use-this': 0,
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Welcome to my api' }));
+routes.get('/', (req, res) => res.json({ message: 'Welcome to MeetApp API.' }));
+routes.post('/users', UserController.store);
 
 export default routes;
