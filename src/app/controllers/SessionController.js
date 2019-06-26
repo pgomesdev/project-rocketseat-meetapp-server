95046b43aa1e035a51342546476997086f521a93
pgomesdev <pgomesdev@gmail.com>
2019-06-26 20:28:31 -0300

    feat: created session endpoint

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    if (!(await user.validatePassword(password))) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+
+    const { id, name } = user;
+
+    const token = jwt.sign({ id: user.id }, process.env.APP_SECRET, {
+      expiresIn: '7d',
+    });
+
+    return res.json({ user: { id, name, email }, token });
+  }
+}
+
+export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to MeetApp API.' }));
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
