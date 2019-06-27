e803cc904f8b5b0343c92ca810a1f70a83ccfad8
pgomesdev <pgomesdev@gmail.com>
2019-06-27 08:09:23 -0300

    feat: added data validation to create session

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,8 +1,21 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Invalid request' });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
