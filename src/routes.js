4672dcd2950bb3fc1e80fdb4e39e879e2246f89a
pgomesdev <pgomesdev@gmail.com>
2019-06-29 15:51:22 -0300

    feat: file upload

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,26 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import auth from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
+// Public Routes
 routes.get('/', (req, res) => res.json({ message: 'Welcome to MeetApp API.' }));
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// Private Routes
 routes.use(auth);
 
+// User
 routes.put('/users', UserController.update);
+
+// File
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
